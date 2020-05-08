cba8ae34de7969117f34fbd228c465a42560bccc
Rodney Cuthbertson <rodney.cuthbertson@coabham.com.au>
2020-05-08 22:03:58 +1000

    prod commit

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 This is a simple game of boggle that can be played by multiple people.
 
+## To Do
+
+* Handle players joining room in the middle of a game
+* Build game flow
+* Destroy rooms if they have no players left
+* Check only unique player names can be added.
+
 ## Architecture
 
 When users hit / they are invited to put in a user name and a room name.
@@ -9,6 +16,33 @@ This submits a POST request which then puts them into the room.
 
 The username must be unique in the room that was created. If the user leaves then comes back the data must be retained while that room is valid. A user joining multiple times will just broadcast the information.
 
+
+
+
+## Program flow
+
+* Server start
+* Client connects and logs in
+* Server creates room and player instances
+* Server sends player data to client
+* Client starts game
+* Server generates game data
+* Client uses interface to create players working entry
+* Client handles legal board moves
+* Client checks that the entries_numbers is unique
+* Client checks working entry is in dictionary
+* Client submits the working_entry to the server
+* Server adds the entry to the player instance
+* Server sends updated player object to the client
+
+
+## Game flow
+
+1. waiting
+2. start
+3. running
+4. stop
+5. waiting
 
 
 ## Player
--- tests/test_game_engine.py
+++ tests/test_game_engine.py
@@ -10,11 +10,11 @@ class TestGameRoomsMethods(unittest.TestCase):
         self.assertEqual(True, server_game_rooms.is_room_active("12345", boggle_room))
 
     def test_get_room(self):
-        br = boggle_room("12345")
+        br = boggle_room("1")
         server_game_rooms = game_rooms()
-        self.assertEqual(None, server_game_rooms.get_room("12345", boggle_room))
+        self.assertEqual(None, server_game_rooms.get_room("1", boggle_room))
         server_game_rooms.active_rooms.append(br)
-        self.assertEqual(server_game_rooms.active_rooms[0], server_game_rooms.get_room("12345", boggle_room))
+        self.assertEqual(server_game_rooms.active_rooms[0], server_game_rooms.get_room("1", boggle_room))
 
 class TestBoggleRoomMethods(unittest.TestCase):
 
@@ -32,3 +32,37 @@ class TestBoggleRoomMethods(unittest.TestCase):
     def test_generate_board(self):
         br = boggle_room("12345")
         self.assertEqual(16, len(br.board))
+
+    def test_get_players_final_scores(self):
+        br = boggle_room("12345")
+        br.players.append(player("matthew"))
+        br.players.append(player("rodney"))
+        br.players.append(player("acacia"))
+        m = br.get_player("matthew")
+        r = br.get_player("rodney")
+        a = br.get_player("acacia")
+
+        a.entries = {
+            'words': ['figs', 'figs', 'spaceship'],
+            'numbers': ['1,2,3,4', '1,3,4,2', '5,4,3,6,2,1,5,3,4'],
+            'points': [1, 1, 10]
+        }
+
+        r.entries = {
+            'words': ['figs', 'apples'],
+            'numbers': ['1,2,3,4', '4,5,2,4,8,16'],
+            'points': [1, 2]
+        }
+
+        m.entries = {
+            'words': ['bedroom'],
+            'numbers': ['4,5,6,7,2,10,11'],
+            'points': [5]
+        }
+
+        test_case = {'acacia': {'entries': {'points': [0, 1, 10],'words': ['figs','figs','spaceship', ]},'total_points': 11},
+                     'matthew': {'entries': {'points': [5], 'words': ['bedroom']}, 'total_points': 5},
+                     'rodney': {'entries': {'points': [0, 2], 'words': ['figs', 'apples',]},'total_points': 2}}
+
+
+        self.assertEqual(test_case, br.get_players_final_scores())
